f546723445cafeb02b92f87b096fc07139c4fe62
z0374 <33524313+z0374@users.noreply.github.com>
2026-05-01 13:04:27 -0300

    Atualizar o warmUp.js

--- src/soundmidi/audio/warmUp.js
+++ src/soundmidi/audio/warmUp.js
@@ -2,7 +2,13 @@ export default function warmUp(audioCtx, buffers, index) {
     if (!buffers[index]) return;
 
     const source = audioCtx.createBufferSource();
+    const gain = audioCtx.createGain();
+
+    gain.gain.value = 0; // 🔇 silencioso
+
     source.buffer = buffers[index];
-    source.connect(audioCtx.destination);
+    source.connect(gain);
+    gain.connect(audioCtx.destination);
+
     source.start(0);
 }
