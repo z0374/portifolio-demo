2c0f41826cac9d7bd87088423c057557fe9cd1a3
z0374 <33524313+z0374@users.noreply.github.com>
2026-05-01 08:25:58 -0300

    Create tracks.js

---
+++ src/soundmidi/config/tracks.js
@@ -0,0 +1,11 @@
+export default [
+    { src: "pom", nome: "Pom", tecla: 7 },
+    { src: "clap", nome: "Clap", tecla: 8 },
+    { src: "tim", nome: "Tim", tecla: 9 },
+    { src: "puff", nome: "Puff", tecla: 4 },
+    { src: "splash", nome: "Splash", tecla: 5 },
+    { src: "toim", nome: "Toim", tecla: 6 },
+    { src: "psh", nome: "Psh", tecla: 1 },
+    { src: "tic", nome: "Tic", tecla: 2 },
+    { src: "tom", nome: "Tom", tecla: 3 }
+];
