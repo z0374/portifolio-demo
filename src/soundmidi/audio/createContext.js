6d6d1d09341f0974d0e605d867044f3fb4b26853
z0374 <33524313+z0374@users.noreply.github.com>
2026-05-01 13:23:06 -0300

    Atualizar o createContext.js

--- src/soundmidi/audio/createContext.js
+++ src/soundmidi/audio/createContext.js
@@ -1,17 +1,5 @@
 export default function createContext() {
-    const audioCtx = new (window.AudioContext || window.webkitAudioContext)({
+    return new (window.AudioContext || window.webkitAudioContext)({
         latencyHint: 'interactive'
     });
-
-    // 🎚️ master gain (volume global)
-    const masterGain = audioCtx.createGain();
-    masterGain.gain.value = 1;
-
-    // 🔗 conecta uma vez só
-    masterGain.connect(audioCtx.destination);
-
-    return {
-        audioCtx,
-        masterGain
-    };
 }
