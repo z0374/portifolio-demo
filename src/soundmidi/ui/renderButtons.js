2fe3ed3b5a9ac55f39cecb35d05e3ed65f6093f2
z0374 <33524313+z0374@users.noreply.github.com>
2026-05-01 08:58:25 -0300

    Atualizar o renderButtons.js

--- src/soundmidi/ui/renderButtons.js
+++ src/soundmidi/ui/renderButtons.js
@@ -1,7 +1,7 @@
-export default function renderButtons(app, tracks, colors) {
+export default function renderButtons(app, tracks) {
     for (let i = 0; i < tracks.length; i++) {
         app.innerHTML += `
-            <button id="b${i}" style="background:${colors[i]}">
+            <button id="b${i}" disabled class="loading">
                 ${tracks[i].nome}
             </button>
         `;
