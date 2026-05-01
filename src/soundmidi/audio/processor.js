8204ecb12298a9c9681e1fb3b70ff2e6bf3f7189
z0374 <33524313+z0374@users.noreply.github.com>
2026-05-01 13:20:37 -0300

    Criar processor.js

---
+++ src/soundmidi/audio/processor.js
@@ -0,0 +1,55 @@
+class SamplerProcessor extends AudioWorkletProcessor {
+
+    constructor() {
+        super();
+
+        this.samples = {};
+        this.playQueue = [];
+
+        this.port.onmessage = (event) => {
+            const { type, data } = event.data;
+
+            if (type === 'load') {
+                this.samples[data.id] = data.buffer;
+            }
+
+            if (type === 'play') {
+                this.playQueue.push({
+                    id: data.id,
+                    pos: 0
+                });
+            }
+        };
+    }
+
+    process(inputs, outputs) {
+        const output = outputs[0];
+
+        for (let channel = 0; channel < output.length; channel++) {
+            const channelData = output[channel];
+
+            for (let i = 0; i < channelData.length; i++) {
+                let sampleValue = 0;
+
+                for (let j = this.playQueue.length - 1; j >= 0; j--) {
+                    const item = this.playQueue[j];
+                    const buffer = this.samples[item.id];
+
+                    if (!buffer) continue;
+
+                    if (item.pos < buffer.length) {
+                        sampleValue += buffer[item.pos++];
+                    } else {
+                        this.playQueue.splice(j, 1);
+                    }
+                }
+
+                channelData[i] = sampleValue;
+            }
+        }
+
+        return true;
+    }
+}
+
+registerProcessor('sampler-processor', SamplerProcessor);
