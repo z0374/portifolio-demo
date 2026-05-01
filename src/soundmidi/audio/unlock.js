7448d6ddcc16b32f3bbacbdaaf639bc14f3001a0
z0374 <33524313+z0374@users.noreply.github.com>
2026-05-01 13:05:51 -0300

    Atualizar o unlock.js

--- src/soundmidi/audio/unlock.js
+++ src/soundmidi/audio/unlock.js
@@ -1,5 +1,12 @@
-export default function unlock(audioCtx) {
-    if (audioCtx.state === 'suspended') {
-        audioCtx.resume();
+document.addEventListener('pointerdown', async () => {
+    if (audioCtx.state !== 'running') {
+        await audioCtx.resume();
+
+        // 🔥 hack: tocar buffer vazio
+        const buffer = audioCtx.createBuffer(1, 1, 22050);
+        const source = audioCtx.createBufferSource();
+        source.buffer = buffer;
+        source.connect(audioCtx.destination);
+        source.start(0);
     }
-}
+}, { once: true });
