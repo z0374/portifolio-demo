f6fc01893e56122999faf43fb72cc4ac0b53b52b
z0374 <33524313+z0374@users.noreply.github.com>
2026-05-01 13:22:45 -0300

    Atualizar o playSound.js

--- src/soundmidi/audio/playSound.js
+++ src/soundmidi/audio/playSound.js
@@ -1,12 +1,6 @@
-export default function playSound(audioCtx, masterGain, buffers, index) {
-    if (!buffers[index]) return;
-
-    const source = audioCtx.createBufferSource();
-    source.buffer = buffers[index];
-
-    source.connect(masterGain);
-
-    // 🔥 pequeno offset elimina latência perceptível
-    const now = audioCtx.currentTime;
-    source.start(now + 0.005);
+export default function playSound(workletNode, index) {
+    workletNode.port.postMessage({
+        type: 'play',
+        data: { id: index }
+    });
 }
