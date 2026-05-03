687b10bb7bd0971831812571ce79682330cf81a7
z0374 <33524313+z0374@users.noreply.github.com>
2026-05-03 08:06:07 -0300

    Atualizar o colors.js

--- src/soundmidi/config/colors.js
+++ src/soundmidi/config/colors.js
@@ -1,4 +1,11 @@
 export default [
-    '#ff6666','#00ccff','#ff00ff','#ff9900',
-    '#ff0099','#ccff33','#ffff00','#ff33cc','#33ff33'
+    '#0D1B2A', // azul profundo (base)
+    '#1A1A40', // roxo escuro
+    '#2B0A3D', // roxo neon escuro
+    '#003B44', // ciano escuro
+    '#1F0036', // violeta profundo
+    '#002F2F', // teal escuro
+    '#3A0CA3', // roxo forte
+    '#14213D', // azul petróleo
+    '#240046'  // roxo intenso
 ];
